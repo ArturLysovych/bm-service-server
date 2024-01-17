5c9bda8495c97f98f67b761be6b4d9fd5012948e
ArturLysovych <arturlisovic@gmail.com>
2024-01-17 15:31:29 +0200

    Connected to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '56717082',
+      database: 'bm-service-db',
+      models: [],
+      autoLoadModels: true
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
